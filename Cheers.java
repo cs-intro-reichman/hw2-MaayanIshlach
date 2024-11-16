92b4287f54059bd3e72057f6a8979ee02dfa2b66
Maayan Ishlach <maayanishlach@gmail.com>
2024-11-16 19:28:37 +0200

    try3

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,27 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String str=args[0];
+            int n=Integer.parseInt(args[1]);
+            String letters = "AEFHILMNORSX";
+            String line= "";
+            for (int i = 0; i <str.length(); i++)
+            {
+                if (letters.indexOf(str)==-1) {
+                        line="Give me a "+ str.charAt(i)+": "+str.charAt(i)+"!";
+                        System.out.println(line);
+
+                }
+                else{
+                        line= "Give me an" + str.charAt(i)+": "+str.charAt(i)+"!";
+                        System.out.println(line);
+                }
+
         }
+        System.out.println("What does that spell?");
+        for (int i = 0; i <n; i++)
+            {
+                System.out.println(str+ "!!!");
+            }
+}
 }
