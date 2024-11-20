7fc26aa0a3ea1f64e5b2c9ca6f5f5cc731e66a33
Maayan Ishlach <maayanishlach@gmail.com>
2024-11-20 17:55:40 +0200

    try

--- Collatz.java
+++ Collatz.java
@@ -1,62 +1,38 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
-	public static void main(String args[]) {
-		int n =Integer.parseInt(args[0]);
-		String str=args[1];
-		int count=0;
-		int num;
-		if (str.equals("v")) {
-			for (int i=1;i<n+1;i++)
-		{
-			count=0;
-			if (i%2!=0) {
-				System.out.print(i+" ");
-				count++;
-				num=i*3+1;
-				while (num!=1) {
-					if (num%2==0) {
-						System.out.print(num+" ");
-						count++;
-						num=num/2;
-					}
-					else
-					{
-						System.out.print(num+" ");
-						count++;
-						num=num*3+1;
-					}
-				}
-				System.out.print(num+" ");
-				count++;
-			}
-			else	
-			{
-				System.out.print(i+" ");
-				count++;
-				num=i;
-				while (num!=1) {
-					if (num%2==0) {
-						num=num/2;
-						System.out.print(num+" ");
-						count++;
-					}
-					else
-					{
-						System.out.print(num+" ");
-						count++;
-						num=num*3+1;
-					}
-				}
+    public static void main(String args[]) {
+        int n = Integer.parseInt(args[0]);
+        String str = args[1];
+        int count;
+        int num;
 
-			}
-			System.out.println(" ("+count+")");
-			System.out.println();
-				
-		}
-	
-	}
-	System.out.println("Every one of the first " +n+" hailstone sequences reached 1.");
-}	
-		
+        if (str.equals("v")) {
+            for (int i = 1; i < n + 1; i++) {
+                count = 0;
+
+                // Start of sequence
+                System.out.print(i + " ");
+                count++;
+                num = (i % 2 != 0) ? i * 3 + 1 : i;
+
+                while (num != 1) {
+                    if (num % 2 == 0) {
+                        num = num / 2;
+                    } else {
+                        num = num * 3 + 1;
+                    }
+                    System.out.print(num + " ");
+                    count++;
+                }
+
+                // End of sequence
+                System.out.println("(" + count + ")");
+            }
+        }
+
+        // Final output message
+        System.out.println("Every one of the first " + n + " hailstone sequences reached 1.");
+    }
 }
+
 
